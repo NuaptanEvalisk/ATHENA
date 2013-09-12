NuaptanEvalisk/ATHENA

commit 682fc4bf5234ae018a439de46581d9a1b128906b

LaTeX preview: adding support for export to LaTeX
git-svn-id: svn+ssh://svn.savannah.gnu.org/texmacs/trunk/src@7155 64cb5145-927a-446d-8aed-2fb7b4773692

TeXmacs/progs/convert/latex/tmtex.scm
@@ -1537,6 +1537,12 @@
 	    (display* "TeXmacs] non converted verbatim content: \n" x "\n")
             ""))))
 
+(define (escape-angles l)
+  (cond ((string? l)
+         (string-replace (string-replace l "<less>" "<") "<gtr>" ">"))
+        ((symbol? l) l)
+        (else (map escape-angles l))))
+
 (define (escape-braces l)
   (cond ((string? l) (string-replace (string-replace l "{" "\\{") "}" "\\}"))
         ((symbol? l) l)
@@ -1568,9 +1574,15 @@
 (define (tmtex-code-block s l)
   (set! l (escape-backslashes l))
   (set! l (escape-braces l))
-  (set! s (car (string-decompose s "-")))
+  (set!   (car (string-decompose s "-")))
   (with lang (if (or (== s "verbatim") (== s "code")) '() `((!option ,s)))
     `((!begin* "tmcode" ,@lang) ,(tmtex-verbatim* "" l))))
+
+(define (tmtex-mixed s l)
+  (set! l (escape-angles l))
+  (with bloc (apply string-append
+                    (list-intersperse (cdr (cadr (cadr (cadr l)))) "\n"))
+    `(!verbatim* ,bloc)))
 
 (define (tmtex-number-renderer l)
   (let ((r (cond ((string? l) l)
@@ -2155,6 +2167,8 @@
    (,tmtex-code-block 1))
   ((:or mmx cpp scm shell scilab) (,tmtex-code-inline 1))
 
+  (picture-mixed (,tmtex-mixed 2))
+  (source-mixed (,tmtex-mixed 2))
   (the-index (,tmtex-theindex -1))
   (glossary (,tmtex-glossary 1))
   (glossary-explain (,tmtex-glossary 2))

commit 042235e633a9e888541c8a17b66a9cc11e7f3809

LaTeX export: fix typo introduced in r7155
git-svn-id: svn+ssh://svn.savannah.gnu.org/texmacs/trunk/src@7258 64cb5145-927a-446d-8aed-2fb7b4773692

TeXmacs/progs/convert/latex/tmtex.scm
@@ -1597,7 +1597,7 @@
 (define (tmtex-code-block s l)
   (set! l (escape-backslashes l))
   (set! l (escape-braces l))
-  (set!   (car (string-decompose s "-")))
+  (set! s (car (string-decompose s "-")))
   (with lang (if (or (== s "verbatim") (== s "code")) '() `((!option ,s)))
     `((!begin* "tmcode" ,@lang) ,(tmtex-verbatim* "" l))))
 