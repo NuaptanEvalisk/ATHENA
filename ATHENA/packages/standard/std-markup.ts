f2d18d09238588961f13c7f942c5e93f60d3f26c
Nuaptan <nuaptan@outlook.com>
2026-05-27 20:39:55 +0800

    improve: refine ATHENA wordmark macro
    - Adjust the ATHENA macro letter placement for a cleaner wordmark.
    
    - Keep raised T and lowered E styling while tightening neighboring letters.

--- ATHENA/packages/standard/std-markup.ts
+++ ATHENA/packages/standard/std-markup.ts
@@ -26,7 +26,7 @@
     </src-comment>
   </active*>
 
-  <assign|ATHENA|<macro|<active*|<with|font-series|bold|A<space|-0.25spc><move|<resize|T||||0.7fn>|0fn|0.3fn><space|-0.2spc>H<space|-0.2spc><move|<resize|E||||0.7fn>|0fn|-0.2fn><space|-0.2spc>N<space|-0.25spc>A>>>>
+  <assign|ATHENA|<macro|<active*|<with|font-series|bold|<move|A|0.07fn|0fn><move|<resize|T||||0.7fn>|0fn|0.25fn><move|H|-0.05fn|0fn><move|<resize|E||||0.7fn>|0fn|-0.25fn>NA>>>>
 
   <assign|athena|<macro|<ATHENA>>>
 
